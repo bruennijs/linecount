7d9343a1b68340ff92c66875bfdcc96647df2598
bruennijs <oliver.bruentje@gmx.de>
2013-10-24 17:16:42 +0200

    added getLineCount with promise return + tests

--- test/q.mapfilelinecount.mocha.js
+++ test/q.mapfilelinecount.mocha.js
@@ -5,9 +5,21 @@
 var mocha = require('mocha'),
   Q = require('q'),
   filelib = require('./../lib/filelib'),
-  console = require('console');
+  console = require('console'),
+  should = require('should');
 
 describe('Q promise based map methods', function () {
+
+  it('#getLineCount promise and then assert in then callback', function () {
+    var path = './test/filereader.3_crlf.testfile';
+
+    var sut = new filelib.line.counter.create();
+    var p = sut.getLineCountP(path);
+    p.done(function (count) {
+      count.should.be.equal(3);
+    });
+  });
+
   it('#map file names to promisses not to the line count itself', function () {
     var paths = ['./test/filereader.two_crlf.testfile', './test/filereader.3_crlf.testfile'];
 
@@ -41,9 +53,9 @@ describe('Q promise based map methods', function () {
     });
   });
 
-  it('#linecount with then and cfcall', function () {
+  it('#linecount with then and nfcall', function () {
     var counter = new filelib.line.counter.create();
-    var promise = Q.nfcall(counter.getLineCount, './filereader.two_crlf.testfile');
+    var promise = Q.nfcall(counter.getLineCount, './test/filereader.two_crlf.testfile');
     promise.then(function (count) {
       console.log('promise then result: ' + count);
     });
